96cba6164a62bbc1b1fe6d051cad1d3ad5df8531
jie-426 <1027331661@qq.com>
2025-03-18 22:25:31 +0800

    Delete shipin.py

--- shipin.py
+++
@@ -1,3 +0,0 @@
-from ultralytics import YOLOv10
-model = YOLOv10(r"E:\desktop\yolov10-main\runs\detect\train18\weights\best.pt")
-results = model.predict(source=r'E:\desktop\2222.avi',save=True,save_frames=True)
